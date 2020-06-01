aaa91799986119a47dc62abf31e072cde3945ca0
subcloud-interactive <65906543+subcloud-interactive@users.noreply.github.com>
2020-06-01 23:40:08 +0300

    Update index.php

--- lauta/b/index.php
+++ lauta/b/index.php
@@ -1,12 +1,12 @@
 <?php
   // Tehdään tää ehkä huomenna...
-  include("./php/settings.php");
+  include("../php/settings.php");
 ?>
 <html>
   
   <head>
     <title> <? echo $config['LautaNimi']; ?> </title>
-    <link rel="stylesheet" type="text/css" href="./css/main.css" />
+    <link rel="stylesheet" type="text/css" href="../css/main.css" />
   </head>
   <body>
     <p> Template: Done! :) </p>    
