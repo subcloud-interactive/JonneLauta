2658fbebb3d50921c2b6984c9a17b7950abc97ec
subcloud-interactive <65906543+subcloud-interactive@users.noreply.github.com>
2020-06-01 23:36:11 +0300

    Update index.php

--- lauta/b/index.php
+++ lauta/b/index.php
@@ -1,3 +1,14 @@
 <?php
   // Tehdään tää ehkä huomenna...
 ?>
+<html>
+  
+  <head>
+    <title> <? echo $config['LautaNimi']; ?> </title>
+    <link rel="stylesheet" type="text/css" href="./css/main.css" />
+  </head>
+  <body>
+    <p> Template: Done! :) </p>    
+  </body>
+</html>
+
