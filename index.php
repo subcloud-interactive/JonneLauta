c487878550707a373b0df866bc0df666e7e0e302
subcloud-interactive <65906543+subcloud-interactive@users.noreply.github.com>
2020-06-01 22:45:16 +0300

    index.php
    Lisätty Kotisivu.

---
+++ index.php
@@ -0,0 +1,60 @@
+// JonneLauta
+
+// Notes:
+// CSS To-Do
+
+// Copyright Veeti Simpanen 2020
+
+<?php
+
+  include("./php/settings.php");
+
+?>
+
+<html>
+  <head>
+    <title> JonneLauta <?php echo $LautaVersio; ?> </title>
+    <? // <link rel="stylesheet" type="text/css" href="./css/main.css" /> ?>
+    <meta charset="utf-8" />
+  </head>
+  <body>
+  <div id="wrapper">
+    <div class="header">
+      <h1 style="text-align: center;"> JonneLauta | <?php $LautaVersio ?> </h1>
+    </div>
+    <div class="main">
+      <br />
+      <h2> Laudat: </h2>
+      <br />
+      <table >
+	<tbody>
+		<tr>
+			<td> Lauta </td>
+			<td> Kuvaus </td>
+		</tr>
+		<tr>
+			<td> /b/ </td>
+			<td> Sekalainen </td>
+		</tr>
+      
+    
+		<tr>
+			<td> /a/ </td>
+			<td> Anime </td>
+		</tr>
+      
+      <tr>
+			<td> /tv/ </td>
+			<td> Televisio & Elokuvat </td>
+		</tr>
+	
+      
+	</tbody>
+</table>
+    </div>
+    <div class="footer">
+      <h3 style="text-align:center;"> </h3>
+    </div>
+   </div>
+  </body>
+</html>
