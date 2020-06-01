b2d22c9ff2f8b61f8864770e1f5e4014073a9d33
subcloud-interactive <65906543+subcloud-interactive@users.noreply.github.com>
2020-06-01 23:32:58 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,60 +1,30 @@
-// JonneLauta
-
-// Notes:
-// CSS To-Do
-
-// Copyright Veeti Simpanen 2020
-
 <?php
 
+  // JonneLauta Software
+
+  // Notes:
+  // CSS To-Do
+
+  // Copyright Veeti Simpanen 2020
+
   include("./php/settings.php");
+  $huonoratkaisu = " | ";
+
 
 ?>
 
 <html>
   <head>
-    <title> JonneLauta <?php echo $config['LautaVersio']; ?> </title>
-    <? // <link rel="stylesheet" type="text/css" href="./css/main.css" /> ?>
+    <title> <?php echo $config['LautaNimi']; echo $config['LautaVersio']; ?> </title>
+    <link rel="stylesheet" type="text/css" href="./css/main.css" /> 
     <meta charset="utf-8" />
   </head>
   <body>
-  <div id="wrapper">
-    <div class="header">
-      <h1 style="text-align: center;"> JonneLauta | <?php $LautaVersio ?> </h1>
-    </div>
-    <div class="main">
-      <br />
-      <h2> Laudat: </h2>
-      <br />
-      <table >
-	<tbody>
-		<tr>
-			<td> Lauta </td>
-			<td> Kuvaus </td>
-		</tr>
-		<tr>
-			<td> /b/ </td>
-			<td> Sekalainen </td>
-		</tr>
-      
-    
-		<tr>
-			<td> /a/ </td>
-			<td> Anime </td>
-		</tr>
-      
-      <tr>
-			<td> /tv/ </td>
-			<td> Televisio & Elokuvat </td>
-		</tr>
-	
-      
-	</tbody>
-</table>
-    </div>
-    <div class="footer">
-      <h3 style="text-align:center;"> </h3>
-    </div>
-   </div>
+	  
+ 	<? echo $config['LautaNimi']; /* Alla näkyy miten nuo linkit pistetään */ ?>
+	<br />
+	<a href="./lauta/b/"> /b/ - Sekalainen </a> <!-- Tee A HREF elementti ja laita linkkiin ./lauta/LAUTA_KIRJAIN/. Sitten /LAUTA_KIRJAIN/ - LAUDAN_KUVAUS -->
+	<br />
+	  
   </body>
 </html>
