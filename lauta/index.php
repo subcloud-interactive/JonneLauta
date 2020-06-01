ac191d94f70887ffa87407386cac66e2fa8182d2
subcloud-interactive <65906543+subcloud-interactive@users.noreply.github.com>
2020-06-01 23:04:54 +0300

    Create index.php

---
+++ lauta/index.php
@@ -0,0 +1,17 @@
+<?php
+  include("./php/settings.php");
+?>
+
+<html>
+
+  <head>
+  
+  </head>
+  
+  <body>
+    <h3 style="text-align: center;"> Redirecting... </h3>
+    <meta http-equiv="refresh" content="3;URL='$config['LautaURL'];'" />   
+    <hr />
+  </body>
+
+</html>
