d07c27177afe9d79a28203ce43f1e85fa6e893c3
subcloud-interactive <65906543+subcloud-interactive@users.noreply.github.com>
2020-06-02 10:45:29 +0300

    Update index.php

--- lauta/b/index.php
+++ lauta/b/index.php
@@ -1,6 +1,11 @@
 <?php
-  // Tehdään tää ehkä huomenna...
+
+
   include("../../php/settings.php");
+  
+  $sql = "SELECT * FROM post_b"; // Huomio: Jokaiselle laudalle pitää olla oma table tietokannassa, esimerkiksi: post_b tai post_a.
+  $result = $connection->query($sql);
+
 ?>
 <html>
   
@@ -9,7 +14,30 @@
     <link rel="stylesheet" type="text/css" href="../../css/main.css" />
   </head>
   <body>
-    <p> Template: Done! :) </p>    
+    <h2 style="text-align:center;"> /b/ - Sekalainen </h2>
+    <hr />
+    <h4> Postaa laudalle: </h4> <br/>
+    
+    <br />
+    <table>
+      <tr>
+        <th> Nimi </th>
+        <th> Viesti </th>
+        <th> Kuva </th>
+      </tr>
+      <?php
+        // Hae tiedot tietokannasta
+        if($result->num_rows > 0) {
+          while($row = $result->fetch_assoc()){
+           echo '<tr>';
+             echo '<td> $row['Name']; </td>';
+             echo '<td> $row['Post']; </td>';
+             echo '<td> $row['Kuva']; </td>';
+           echo '</tr>';
+          }
+        }
+      ?>
+    </table>
   </body>
 </html>
 
