0c92eaa739f9cf716b38465faa59e3265289f500
subcloud-interactive <65906543+subcloud-interactive@users.noreply.github.com>
2020-06-02 10:49:18 +0300

    Update settings.php

--- php/settings.php
+++ php/settings.php
@@ -1,11 +1,11 @@
 <?php
   
-  /*
+ 
   $config['Palvelin'] = "localhost"; //Tietokantasi Hosti esim: localhost tai 127.0.0.1
   $config['TietokantaKäyttäjä'] = "Tietokannan Käyttäjä"; //Tietokantasi Käyttäjänimi (esim. root)
   $config['TietokantaSalasana'] = "Tietokannan Salasana"; //Tietokantasi salasana.
   $config['Tietokanta'] = "Tietokannan Nimi"; //Tietokantasi nimi.
-  */
+  
   $config['LautaNimi'] = "Laudan Nimi"; //Tähän tulee lautasi nimi.
   $config['LautaVersio'] = "Laudan Versio"; //Vaihda aina kun päivität lautaasi. Vinkki: Kannattaa aloittaa versiosta 0.1
   $config['LautaURL'] = "https://example.com/"; //Lautasi *KOKO* URL osoite. Esim: https://example.com/ (HUOM: käytä https protokollaa, jos sinulla on SSL, muissa tapauksissa käytä HTTP.)
@@ -16,7 +16,7 @@
 
 
   
-/*  $conn = mysqli_connect($Palvelin, $TietokantaKäyttäjä, $TietokantaSalasana, $Tietokanta); - Pois Käytöstä, ei valmis! */
+$conn = mysqli_connect($config['Palvelin'], $config['TietokantaKäyttäjä'], $config['TietokantaSalasana'], $config['Tietokanta']);
   
   
   
