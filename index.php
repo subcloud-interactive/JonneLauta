befcb8807036bc456090c2591b752b5ba893b870
subcloud-interactive <65906543+subcloud-interactive@users.noreply.github.com>
2020-06-02 12:25:49 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,21 @@
 <?php
 
-  // JonneLauta Software
+  /*
+  
+       _                        _                 _        
+     | |                      | |               | |       
+     | | ___  _ __  _ __   ___| |     __ _ _   _| |_ __ _ 
+ _   | |/ _ \| '_ \| '_ \ / _ \ |    / _` | | | | __/ _` |
+| |__| | (_) | | | | | | |  __/ |___| (_| | |_| | || (_| |
+ \____/ \___/|_| |_|_| |_|\___|______\__,_|\__,_|\__\__,_|
+  
+  
+  
+  
+  */
+  
 
-  // Notes:
-  // CSS To-Do
-
-  // Copyright Veeti Simpanen 2020
+  // COPYRIGHT Subcloud Interactive 2020
 
   include("./php/settings.php");
   $huonoratkaisu = " | ";
